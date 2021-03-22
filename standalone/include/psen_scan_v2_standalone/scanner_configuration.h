657d1ec1af0fe95df04403881c131e4fa8e12627
Immanuel Martini <i.martini@pilz.de>
2021-03-22 16:50:25 +0100

    Workaround for false-positive warning using boost::optional
    see https://www.boost.org/doc/libs/1_65_1/libs/optional/doc/html/boost_optional/tutorial/gotchas/false_positive_with__wmaybe_uninitialized.html

--- standalone/include/psen_scan_v2_standalone/scanner_configuration.h
+++ standalone/include/psen_scan_v2_standalone/scanner_configuration.h
@@ -19,6 +19,7 @@
 
 #include "psen_scan_v2_standalone/configuration/default_parameters.h"
 #include "psen_scan_v2_standalone/scan_range.h"
+#include "psen_scan_v2_standalone/util/tenth_of_degree.h"
 
 namespace psen_scan_v2_standalone
 {
@@ -57,15 +58,17 @@ private:
   bool isValid() const;
 
 private:
-  boost::optional<uint32_t> host_ip_;
+  boost::optional<uint32_t> host_ip_{ boost::make_optional(false, uint32_t()) };
   uint16_t host_data_port_{ configuration::DATA_PORT_OF_HOST_DEVICE };
   uint16_t host_control_port_{ configuration::CONTROL_PORT_OF_HOST_DEVICE };
 
-  boost::optional<uint32_t> scanner_ip_;
+  boost::optional<uint32_t> scanner_ip_{ boost::make_optional(false, uint32_t()) };
   uint16_t scanner_data_port_{ configuration::DATA_PORT_OF_SCANNER_DEVICE };
   uint16_t scanner_control_port_{ configuration::CONTROL_PORT_OF_SCANNER_DEVICE };
 
-  boost::optional<ScanRange> scan_range_{};
+  boost::optional<ScanRange> scan_range_{
+    boost::make_optional(false, ScanRange(util::TenthOfDegree(0), util::TenthOfDegree(0)))
+  };
   bool diagnostics_enabled_{ false };
 };
 
